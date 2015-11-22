b45b28870f7ac7eb1b5c4e4887a69abbe691296e
Graig Janssen <graig.janssen@gmail.com>
2015-11-22 10:01:39 -0500

    Creates HTML structure, CSS Styles, JS wireframe

--- scripts.js
+++ scripts.js
@@ -1,1 +1,43 @@
 console.log("...loaded")
+
+var dice = [
+  ["A","A","E","E","G","N"],
+  ["E","L","R","T","T","Y"],
+  ["A","O","O","T","T","W"],
+  ["A","B","B","J","O","O"],
+  ["E","H","R","T","V","W"],
+  ["C","I","M","O","T","U"],
+  ["D","I","S","T","T","Y"],
+  ["E","I","O","S","S","T"],
+  ["D","E","L","R","V","Y"],
+  ["A","C","H","O","P","S"],
+  ["H","I","M","N","Q","U"],
+  ["E","E","I","N","S","U"],
+  ["E","E","G","H","N","W"],
+  ["A","F","F","K","P","S"],
+  ["H","L","N","N","R","Z"],
+  ["D","E","I","L","R","X"]
+]
+
+function setTimer(limit) {
+  // Sets timer depending on button clicked.
+  // Displays countdown above board-area
+  // Stops gameplay when time runs out
+}
+
+function setBoard() {
+  // generates random boggle board
+}
+
+function makeWord() {
+  // Listens for clicked cells
+  // Adds clicked letters to field below board
+  // Starts new word when non-adjacent square is clicked
+}
+
+function submitWord() {
+  // Listens for ENTER key
+  // Calculates score of submitted word
+  // Adds score to score-display
+  // Adds word to word list
+}
